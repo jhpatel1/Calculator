7846a985da4439385282a5378737063bb1a3717e
Jatin Patel <93479087+jhpatel1@users.noreply.github.com>
2023-02-20 09:46:13 -0600

    -GUI for calculator built -Buttons actions not implemented yet

---
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -0,0 +1,61 @@
+package com.example.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+    public TextField displayTextField;
+
+    public void seven(ActionEvent actionEvent) {
+    }
+
+    public void eight(ActionEvent actionEvent) {
+    }
+
+    public void nine(ActionEvent actionEvent) {
+    }
+
+    public void multiply(ActionEvent actionEvent) {
+    }
+
+    public void four(ActionEvent actionEvent) {
+    }
+
+    public void five(ActionEvent actionEvent) {
+    }
+
+    public void six(ActionEvent actionEvent) {
+    }
+
+    public void divide(ActionEvent actionEvent) {
+    }
+
+    public void one(ActionEvent actionEvent) {
+    }
+
+    public void two(ActionEvent actionEvent) {
+    }
+
+    public void three(ActionEvent actionEvent) {
+    }
+
+    public void subtract(ActionEvent actionEvent) {
+    }
+
+    public void zero(ActionEvent actionEvent) {
+    }
+
+    public void point(ActionEvent actionEvent) {
+    }
+
+    public void enter(ActionEvent actionEvent) {
+    }
+
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void changeSign(ActionEvent actionEvent) {
+    }
+}
